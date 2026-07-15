d886d3abf8ade478dddacb968fbdac461d5b1a56
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-07-15 13:06:37 +0530

    update

--- lec1.java
+++ lec1.java
@@ -36,7 +36,7 @@ public class lec1{
         }
         int maxElem = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (maxElem < arr[i + 1]) {
+            if (arr[i] < arr[i + 1]) { 
                 maxElem = arr[i + 1];
             }
             else {
